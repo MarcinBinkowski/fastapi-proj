650e34895a34da92db4cd646f4b8223a1eebbedb
MarcinBinkowski <MarcinBinkowski@users.noreply.github.com>
2021-10-25 21:23:51 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="MarcinBinkowski"
+original_name="fastapi_proj"
+original_urlname="fastapi-proj"
+original_description="Awesome fastapi_proj created by MarcinBinkowski"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi_proj $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
